9423aec03b38d1eae308a82d2ca5d1c3dd6b5bd1
Aadit <baggaaadit017@gmail.com>
2021-07-16 19:32:06 +0530

    last commit of 16 july 21

--- oneliner.java
+++ oneliner.java
@@ -10,13 +10,13 @@ import java.util.stream.Stream;
 public class oneliner {
     public static void main(String args[]){ 
         //! fizz buzz in a single line 
-          //  IntStream.rangeClosed(1, 10).forEach(i -> System.out.println((i % 3 == 0) ? ((i % 5 == 0) ? "FizZzBuzz" : "Fizz") : (i % 5 == 0) ? "Buzz" : i));
+            IntStream.rangeClosed(1, 10).forEach(i -> System.out.println((i % 3 == 0) ? ((i % 5 == 0) ? "FizZzBuzz" : "Fizz") : (i % 5 == 0) ? "Buzz" : i));
         //! factorial in a single line 
-        //  int n=10;
-        //  IntStream.rangeClosed(1, n).filter(i-> n%i==0).forEach(i-> System.out.println(i)); 
+        int n=10;
+        IntStream.rangeClosed(1, n).filter(i-> n%i==0).forEach(i-> System.out.println(i)); 
         //! string reverse in java in a single line 
-        // String name="Aadit singh bagga";
-        // IntStream.rangeClosed(0, name.length()-1).forEach(i-> System.out.print(name.charAt((name.length()-1)-i)));
+        String name="Aadit singh bagga";
+        IntStream.rangeClosed(0, name.length()-1).forEach(i-> System.out.print(name.charAt((name.length()-1)-i)));
         // ! trying for fibonnica one liner 
         Stream.iterate(new int[]{0,1} , t -> new int[]{t[1],t[0]+t[1]})
         .limit(10)
